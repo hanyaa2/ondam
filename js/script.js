781fa0b8b7f15bf5866c26bce50e7a0160f04599
hanyaa2 <hananyaa21@gmail.com>
2026-03-31 16:17:50 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 // 팝업창===========================================
-    $(function(){
-                $(".popup").fadeIn();
-                $("body").addClass("scroll_none");
-            });
-            $(".Yes").click(function(){
-                $(".popup").fadeOut();
-                $("body").removeClass("scroll_none");
-            });
-                        $(".No").click(function(){
-                $(".popup").fadeOut();
-                $("body").removeClass("scroll_none");
-            });
-        // });
+$(function () {
+  $(".popup").fadeIn();
+  $("body").addClass("scroll_none");
+});
+$(".Yes").click(function () {
+  $(".popup").fadeOut();
+  $("body").removeClass("scroll_none");
+});
+$(".No").click(function () {
+  $(".popup").fadeOut();
+  $("body").removeClass("scroll_none");
+});
 
-
+document.querySelector(".No").addEventListener("click", function () {
+  alert("19세 이상만 이용 가능합니다.");
+});
 
 // 이미지 확대=====================================
 $(window).on('scroll', function () {
@@ -44,13 +45,13 @@ $(function () {
 
 // 상품 스와이퍼========================================
 
- var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      freeMode: true,
-      loop : true,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
